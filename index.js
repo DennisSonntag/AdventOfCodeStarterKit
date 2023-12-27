1957a99eeba5514b942722899b090c6562b996f0
Dennis Sonntag <sonntagdennis8@gmail.com>
2023-12-26 19:05:29 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const fs = require('node:fs/promises');
+import fs from 'node:fs/promises'
+
+async function example() {
+	try {
+		const data = await fs.readFile('./test.txt', { encoding: 'utf8' });
+		console.log(data);
+	} catch (err) {
+		console.log(err);
+	}
+}
+example();
